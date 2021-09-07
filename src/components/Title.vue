589deb1d89192ecc1256ee167501214776cb7612
LAPTOP-QGNQ6M3J\88697 <kate20210906@gmail.com>
2021-09-07 14:45:10 +0800

    add Title component

---
+++ src/components/Title.vue
@@ -0,0 +1,12 @@
+<template>
+    <p 
+      class="font-normal text-2.1xl font-sans text-center">
+        {{heading}}
+    </p>
+</template>
+
+<script>
+export default {
+    props:['heading']
+}
+</script>
