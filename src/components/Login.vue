7e954bc6c5b7ca18b1f9842cf11cbb9a34928841
LAPTOP-QGNQ6M3J\88697 <kate20210906@gmail.com>
2021-09-07 14:46:05 +0800

    add the Login component

---
+++ src/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <Title class="w-full h-12" heading="欢迎登录测试钱包"></Title>
+</template>
+
+<script>
+import Title from "../components/Title.vue"
+
+export default {
+    components:{
+        Title
+    }
+}
+</script>
