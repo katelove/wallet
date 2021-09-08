64e1e01b132f888705f7e1e4c87a769846d63d2f
LAPTOP-QGNQ6M3J\88697 <kate20210906@gmail.com>
2021-09-08 15:28:01 +0800

    install dragVerify

---
+++ src/components/DragVerify.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex justify-center mt-10 flex-col">
+      <h3>滑图验证</h3>
+    <drag-verify-img-chip
+      ref="dragVerify"
+      :width="260"
+      :imgsrc="img"
+      :isPassing.sync="isPassing"
+      :showRefresh="true"
+      text="请按住滑块拖动"
+      successText="验证通过"
+      handlerIcon="el-icon-d-arrow-right"
+      successIcon="el-icon-circle-check"
+      @passcallback="pass"
+    >
+    </drag-verify-img-chip>
+  </div>
+</template>
+
+<script>
+import dragVerifyImgChip from "vue-drag-verify-img-chip";
+export default {
+  data() {
+    return {
+      img: require("../../public/image/verify-img.png"),
+    };
+  },
+  components: {
+    dragVerifyImgChip,
+  },
+  methods:{
+      pass(){
+          
+      }
+  }
+};
+</script>
