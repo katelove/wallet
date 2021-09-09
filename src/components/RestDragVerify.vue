07e885d111819c0879901a847841f3cd135400a0
LAPTOP-QGNQ6M3J\88697 <kate20210906@gmail.com>
2021-09-09 11:30:19 +0800

    modify style

--- src/components/RestDragVerify.vue
+++ src/components/RestDragVerify.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="flex items-center flex-col mt-25 border-melancholyBlue border mr-8 ml-8 pr-11 pl-11">
-      <h3 class="text-25px mt-5">滑图验证</h3>
+  <div
+    class="flex flex-col mt-25 border-melancholyBlue border mr-8 ml-8 pr-11 pl-11"
+  >
+    <!-- <h3 class="text-25px mt-5">滑图验证</h3>
       <div class="flex justify-items-end">
         <img src="../../public/image/x.png">
-      </div>
-    <dragVerify class="mt-2 mb-5"/>
+      </div> -->
+    <div class="relative">
+      <h3 class="text-25px text-center">滑图验证</h3>
+      <img class="absolute right-0 close-top" src="../../public/image/x.png" />
+    </div>
+    <dragVerify class="mt-2 mb-5" />
   </div>
 </template>
 
@@ -16,3 +22,10 @@ export default {
   },
 };
 </script>
+
+<style>
+
+.close-top{
+  transform: translateY(-50%);
+}
+</style>
