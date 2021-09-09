63bfa289a6b9d519b2d851f9e31b3546e3848b9e
LAPTOP-QGNQ6M3J\88697 <kate20210906@gmail.com>
2021-09-09 14:00:55 +0800

    add shadow

--- src/components/RestPwd.vue
+++ src/components/RestPwd.vue
@@ -1,15 +1,15 @@
 <template>
   <div
-    class="flex items-center flex-col mt-25 border-melancholyBlue border mr-8 ml-8 pr-6 pl-6"
+    class="flex items-center flex-col mt-25 border-melancholyBlue border mr-8 ml-8 p-6 rounded border-shadow"
   >
-    <h3 class="text-25px mt-5">重置登录密码</h3>
+    <h3 class="text-25px mb-5">重置登录密码</h3>
     <div class="bg-skyBlue">
       <p class="text-sm text-wordBlue mr-2 ml-2">
         重置登录密码后，24小时内禁止提币和OTC
       </p>
     </div>
-      <Input class="w-full h-8.5 m-6" placeholder="帐号" />
-      <Button class="w-full h-10 m-6">提交</Button>
+      <Input class="w-full h-8.5 mt-9" placeholder=" 帐号" />
+      <Button class="w-full h-10 mt-9">提交</Button>
   </div>
 </template>
 
@@ -24,3 +24,10 @@ export default {
   },
 };
 </script>
+
+<style>
+.border-shadow{
+  box-shadow: 4px 4px 3px rgba(0, 0, 0, 0.4);
+}
+
+</style>
