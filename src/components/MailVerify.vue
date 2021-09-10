b0cf6820f947a2551c780940f80889f0fb540a73
LAPTOP-QGNQ6M3J\88697 <kate20210906@gmail.com>
2021-09-10 14:47:05 +0800

    create mailverify

---
+++ src/components/MailVerify.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Title class="flex justify-center mt-10" heading="邮箱验证"></Title>
+    <div
+      class="flex flex-col border-melancholyBlue border m-8 p-5 rounded border-shadow"
+    >
+      <p class="text-yewLime text-center text-sm">请输入您的邮箱ABCD@GMAIL.com收到的验证码</p>
+      <p class="text-yewLime text-center text-sm mt-2.5">邮箱验证码可能被判定为垃圾邮件，请注意查收。</p>
+      <Input class="w-75 h-8.5 mt-6 text-center text-base" placeholder="邮箱验证码" />
+      <Button class="w-75 h-10 mt-12">确认</Button>
+      <h3 class="mt-6 text-base text-center text-yewLime">没有收到验证码?<span class="text-bayshoreBlue">获取验证码</span></h3>
+    </div>
+  </div>
+</template>
+
+<script>
+import Title from "./Title.vue";
+import Input from "./Input.vue";
+import Button from "./Button.vue";
+export default {
+  components: {
+    Title,
+    Input,
+    Button,
+  },
+};
+</script>
+
+<style>
+.border-shadow {
+  box-shadow: 4px 4px 3px rgba(0, 0, 0, 0.4);
+}
+</style>
