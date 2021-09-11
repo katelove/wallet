8e6dbca98c87c859cece06c8a53e9bdcbddebb5e
LAPTOP-QGNQ6M3J\88697 <kate20210906@gmail.com>
2021-09-11 14:14:03 +0800

    add all content

--- src/components/ModifyLoginPwd.vue
+++ src/components/ModifyLoginPwd.vue
@@ -9,21 +9,53 @@
             class="w-full border-none border-line  mt-8"
             placeholder="请输入当前登录密码"
           />
-          <img class="absolute right-0 top-6" src="../../public/image/eye.png" />
+          <img
+            class="absolute right-0 top-6"
+            src="../../public/image/eye.png"
+          />
         </div>
       </div>
       <div class="w-full bg-diamondGrey h-2"></div>
-          <div class="pr-6 pl-6 pt-5 pb-5">
+      <div class="pr-6 pl-6 pt-5 pb-5">
         <p class="text-base">新登录密码</p>
         <div class="relative">
           <input
             class="w-full border-none border-line  mt-8"
             placeholder="请输入新登录密码"
           />
-          <img class="absolute right-0 top-6" src="../../public/image/eye.png" />
+          <img
+            class="absolute right-0 top-6"
+            src="../../public/image/eye.png"
+          />
         </div>
       </div>
       <div class="w-full bg-diamondGrey h-2"></div>
+      <div class="pr-6 pl-6 pt-5 pb-5">
+        <p class="text-base">确认登录密码</p>
+        <div class="relative">
+          <input
+            class="w-full border-none border-line  mt-8"
+            placeholder="请再次输入新登录密码"
+          />
+          <img
+            class="absolute right-0 top-6"
+            src="../../public/image/eye.png"
+          />
+        </div>
+      </div>
+      <div class="w-full bg-diamondGrey h-2"></div>
+      <div class="pr-6 pl-6 pt-5 pb-5">
+        <p class="text-base">手机验证码</p>
+        <div class="relative">
+          <input
+            class="w-full border-none border-line  mt-8"
+            placeholder="请输入手机验证码"
+          />
+      <Button class="h-8 absolute right-2 bottom-2 rounded-5px p-1.5 ">获取验证码</Button>
+        </div>
+      </div>
+      <div class="w-full bg-diamondGrey h-2"></div>
+       <Button class="w-75 h-10 m-6 rounded-10px">提交</Button>
     </BlueContainer>
   </div>
 </template>
@@ -31,13 +63,13 @@
 <script>
 import Title from "../components/Title.vue";
 import BlueContainer from "../components/BlueContainer.vue";
-// import Input from "../components/Input.vue"
+import Button from "../components/Button.vue";
 
 export default {
   components: {
     Title,
     BlueContainer,
-    // Input
+    Button,
   },
 };
 </script>
