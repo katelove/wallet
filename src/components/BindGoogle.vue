b88fc1e5c0108b3b734efa4d4a7861f57a9dc4a6
LAPTOP-QGNQ6M3J\88697 <kate20210906@gmail.com>
2021-09-11 16:13:28 +0800

    modify style

--- src/components/BindGoogle.vue
+++ src/components/BindGoogle.vue
@@ -7,22 +7,26 @@
           1.下载Google Authenticator［谷歌验证器］
         </p>
         <div class="flex justify-center m-4">
-          <Button class="w-32 text-center rounded-md">App Store</Button>
+          <Button class="w-32 text-center rounded-md text-sm">App Store</Button>
         </div>
         <p class="text-yewLime text-xs">
-          2.打開谷歌驗證器，掃描下方二維碼或手動輸入下述密鑰添加驗證令牌
+          2.打開谷歌驗證器，掃描下方二維碼或手動輸入下述密鑰添加驗證令牌<br>
           密鑰用於手機更換或遺失時找回谷歌驗證器，綁定前請務必將下述密鑰備份保存
         </p>
         <div class="flex justify-center mt-4 mb-6">
-        <img src="../../public/image/google-qr-code.png" />
+          <img src="../../public/image/google-qr-code.png" />
+        </div>
+        <div class="flex justify-center m-4">
+        <Button class="w-64 text-center rounded-lg text-xs h-8"
+          >打開Google Authenticator 進行添加</Button
+        >
         </div>
 
-        <Button>打開Google Authenticator 進行添加</Button>
-        <div class="flex flex-start mt-3.5">
-          <p class="text-bayshoreBlue text-sm">GF32ADWE32112FDS3DA1</p>
+        <div class="flex justify-center items-center mt-4">
+          <p class="text-bayshoreBlue text-sm mr-5px font-sans tracking-wider not-italic font-normal">GF32ADWE32112FDS3DA1</p>
           <img src="../../public/image/copy.png" class="mr-5px" />
         </div>
-        <Button class="w-75 h-10 m-6 rounded-10px">下一步</Button>
+        <Button class="w-75 h-10 mt-4 rounded-10px">下一步</Button>
       </div>
     </BlueContainer>
   </div>
