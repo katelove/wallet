0850e55036e17174eddf2c530ae413d824c93c10
LAPTOP-QGNQ6M3J\88697 <kate20210906@gmail.com>
2021-09-12 14:55:38 +0800

    install vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
-
+Vue.use(Vuex)
 new Vue({
     router,
     render: h => h(App),
