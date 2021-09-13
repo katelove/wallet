7cdaeac94cd5ec97f8e0eda878232197d3e9f65b
LAPTOP-QGNQ6M3J\88697 <kate20210906@gmail.com>
2021-09-13 18:36:54 +0800

    add safeCenter router

--- src/router/index.js
+++ src/router/index.js
@@ -34,6 +34,36 @@ const routes = [
       name: "NameWalletPage",
       component: () => import("@/views/Login/Wallet/NameWalletPage.vue"),
     },
+    {
+      path: '/safeCenter',
+      name: "SafeCenter",
+      component: () => import("@/views/Login/SafeCenter/SafeCenter.vue"),
+    },
+    {
+      path: '/safeCenter/modifyPwd',
+      name: "ModifyLoginPwd",
+      component: () => import("@/views/Login/SafeCenter/ModifyLoginPwd.vue"),
+    },
+    {
+      path: '/safeCenter/modifySecondPwd',
+      name: "ModifyLoginSecondPwd",
+      component: () => import("@/views/Login/SafeCenter/ModifyLoginSecondPwd.vue"),
+    },
+    {
+      path: '/safeCenter/setSecondPwd',
+      name: "SetSecondPwd",
+      component: () => import("@/views/Login/SafeCenter/SetSecondPwd.vue"),
+    },
+    {
+      path: '/safeCenter/bindPhone',
+      name: "BindPhone",
+      component: () => import("@/views/Login/SafeCenter/BindPhone.vue"),
+    },
+    {
+      path: '/safeCenter/bindGoogle',
+      name: "BindGooglePage",
+      component: () => import("@/views/Login/SafeCenter/BindGooglePage.vue"),
+    },
 ];
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,74 +1,15 @@
 <template lang="">
-  <!-- <NewPwd /> -->
-  <!-- <RestDragVerify/> -->
-  <!-- <RestPwd/> -->
-  <!-- <SafeVerify/> -->
-  <!-- <TestWallet /> -->
-  <!-- <LoginSafeVerify/> -->
-  <!-- <LoginDragVerify/> -->
-  <!-- <PhoneVerify/> -->
-  <!-- <RegisterTestWallet/> -->
-  <!-- <MailVerify/> -->
-  <!-- <NameTestWallet/> -->
-  <!-- <NameVerify/> -->
-  <!-- <SwitchBtn/> -->
-  <!-- <SafeCenter/> -->
-  <!-- <ModifyLoginPwd/> -->
-  <!-- <ModifyLoginSecondPwd/> -->
-  <!-- <SetSecondPwd/> -->
-  <!-- <BindPhone/> -->
-  <!-- <BindGoogle/> -->
-  <!-- <ModalDragVerify/> -->
-  <BindSecondGoogle/>
+  <Title />
 </template>
 
 <script>
-// import NewPwd from "../components/NewPwd.vue";
-//  import RestDragVerify from "../components/RestDragVerify.vue";
-// import RestPwd from "../components/RestPwd.vue";
-// import SafeVerify from "../components/SafeVerify.vue";
-// import TestWallet from "../components/TestWallet.vue";
-// import LoginSafeVerify from "../components/LoginSafeVerify.vue";
-// import LoginDragVerify from "../components/LoginDragVerify.vue";
-// import PhoneVerify from "../components/PhoneVerify.vue";
-// import RegisterTestWallet from "../components/RegisterTestWallet.vue";
-// import MailVerify from "../components/MailVerify.vue";
-// import NameTestWallet from "../components/NameTestWallet.vue"
-// import NameVerify from "../components/NameVerify.vue"
-// import SwitchBtn from "../components/SwitchBtn.vue"
-// import SafeCenter from "../components/SafeCenter.vue"
-// import ModifyLoginPwd from "../components/ModifyLoginPwd.vue"
-// import ModifyLoginSecondPwd from "../components/ModifyLoginSecondPwd.vue"
-// import SetSecondPwd from "../components/SetSecondPwd.vue";
-// import BindPhone from "../components/BindPhone.vue"
-// import BindGoogle from "../components/BindGoogle.vue"
-// import ModalDragVerify from "../components/ModalDragVerify.vue"
-import BindSecondGoogle from "@/components/BindSecondGoogle.vue"
+import Title from "@/components/Title.vue";
 
 export default {
+  name: "Home",
   components: {
-    // NewPwd,
-    //  RestDragVerify,
-    // RestPwd,
-    // SafeVerify,
-    // TestWallet,
-    // LoginSafeVerify,
-    // LoginDragVerify
-    // PhoneVerify 
-    // RegisterTestWallet
-    // MailVerify
-    // NameTestWallet
-    // NameVerify
-    // SwitchBtn
-    // SafeCenter
-    // ModifyLoginPwd
-    // ModifyLoginSecondPwd
-    // SetSecondPwd
-    // BindPhone
-    // BindGoogle
-    // ModalDragVerify,
-    BindSecondGoogle
+    Title,
   },
-}; 
+};
 </script>
 <style lang=""></style>
