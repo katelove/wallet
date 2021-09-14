1acf6a5c1cb0842c68419773ea9519f5cc322664
LAPTOP-QGNQ6M3J\88697 <kate20210906@gmail.com>
2021-09-14 09:30:01 +0800

    change cross path and redirect home page

--- src/views/Login/Reset/NewPwd.vue
+++ src/views/Login/Reset/NewPwd.vue
@@ -4,31 +4,37 @@
   >
     <div class="relative">
       <h3 class="text-25px text-center">新密碼</h3>
-      <img class="absolute right-0 close-top" src="../../../../public/image/x.png" />
+      <img class="absolute right-0 close-top" src="@/assets/icon/cross.png" />
     </div>
     <div class="relative">
-      <Input class="w-full h-8.5 mt-7.5 " placeholder=" 新登录密码" />
+      <Input type="password" class="w-full h-8.5 mt-7.5 " placeholder=" 新登录密码" />
       <img
         class="absolute right-6 new-top"
-        src="../../../../public/image/more.png"
+        src="@/assets/icon/more.png"
       />
     </div>
     <label class="text-yewLime text-xs mt-7.5">8-20位字符 不能为纯数字</label>
-    <Input class="w-full h-8.5 mt-2" placeholder=" 确认新密码" />
+    <Input type="password" class="w-full h-8.5 mt-2" placeholder=" 确认新密码" />
     <div class="flex justify-end">
-      <Button class="mt-12 w-full h-8 rounded-2px">提交</Button>
+      <button class="bg-melancholyBlue text-white rounded-2px mt-12 w-full h-8" @click="home">提交</button>
+      <!-- <Button class="mt-12 w-full h-8 rounded-2px" @click="home">提交</Button> -->
     </div>
   </div>
 </template>
 
 <script>
 import Input from "@/components/Input.vue";
-import Button from "@/components/Button.vue";
+// import Button from "@/components/Button.vue";
 export default {
   name:'NewPwd',
+  methods:{
+     home(){
+        this.$router.push('/')
+     }
+  },
   components: {
     Input,
-    Button,
+    // Button,
   },
 };
 </script>
