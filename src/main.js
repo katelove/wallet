a565e0692ea9026d0bdcb8f1b0e02e94905edb84
henrychaung <henry.chaung@blocktown.one>
2021-09-28 09:51:37 +0800

    1. Implement vue-clipboard2

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import { Plugin } from 'vue-fragment'
 import store from './store/store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
+import VueClipboard from 'vue-clipboard2'
 
+Vue.use(VueClipboard)
 Vue.use(Plugin)
 Vue.use(require('vue-moment'))
 Vue.config.productionTip = false
