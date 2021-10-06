34829c4de14e1e5c4d2ba9e386828c1f2fe23d5d
LAPTOP-QGNQ6M3J\88697 <kate20210906@gmail.com>
2021-10-06 18:08:20 +0800

    fix: update code

--- src/views/Wallet/index.vue
+++ src/views/Wallet/index.vue
@@ -1,23 +1,17 @@
+/* eslint-disable vue/no-side-effects-in-computed-properties */
 <template lang="">
     <div class="my-2">
         <div class="bg-mainColor rounded-[12px] flex flex-col items-start justify-center pl-[28px] pt-[16px] pb-[16px] mx-[12px]">
             <p class="font-bold text-white text-[18px]">
                 總資產折合 (USDT)
             </p>
-            <div class="relative">
-                <div class="mr-[25px]">
-                    <input
-                        v-model="amountTotal"
-                        :type="type"
-                        v-bind="$attrs"
-                        disabled="disabled"
-                        class="text-[16px] text-white border-0 bg-mainColor w-[101px]"
-                    >
-                </div>
+            <div class="flex items-center justify-start">
+                <p class="text-[16px] text-white border-0 bg-mainColor w-[101px]">
+                    {{ amountTotal }}
+                </p>
                 <Icon
-                    class="absolute right-0 top-1/2 -translate-y-2/4"
+                    class="w-[16px] h-[16px]"
                     :src="eyePic"
-                    style="width: 16px; height: 16px"
                     @click="toggle"
                 />
             </div>
@@ -25,8 +19,7 @@
         <div class="flex items-center justify-center mt-[16px] mb-[11px] text-[12px]">
             <div class="w-[53px] h-[60px]">
                 <Icon
-                    class="m-auto"
-                    style="width: 32px; height: 32px"
+                    class="m-auto w-[32px] h-[32px]"
                     :src="require('@/assets/icon/deposit.png')"
                 />
                 <p class="mt-[8px] text-center">
@@ -76,7 +69,7 @@ export default {
     data() {
         return {
             eyePic: require("@/assets/icon/asset_open_eye.png"),
-            type: this.$attrs.type || "text",
+            wordDisplay: true,
         };
     },
     computed: {
@@ -84,15 +77,21 @@ export default {
             return this.$store.getters.balances;
         },
         amountTotal() {
-            return this.$store.getters.amountTotal;
+            if (this.wordDisplay === false) {
+                return "*********";
+            } else {
+                return this.$store.getters.amountTotal;
+            }
         },
     },
     methods: {
         toggle() {
-            this.type = this.type === "text" ? "password" : "text";
-            this.eyePic = require("@/assets/icon/asset_open_eye.png");
-            if (this.type === "password") {
+            if (this.wordDisplay === true) {
+                this.wordDisplay = false;
                 this.eyePic = require("@/assets/icon/asset_eye.png");
+            } else {
+                this.wordDisplay = true;
+                this.eyePic = require("@/assets/icon/asset_open_eye.png");
             }
         },
     },
